d9889d5dd087dabeaf1f663eb2c85a84b1b251f5
Vadde Yuvaraj <yuvarajyuvi670@gmail.com>
2026-03-09 12:15:24 +0530

    Added delete task and JSON storage feature

--- main.py
+++ main.py
@@ -8,27 +8,34 @@ def main():
         print("\nTask Manager")
         print("1. Add Task")
         print("2. View Tasks")
-        print("3. Mark Task Complete")
+        print("3. Delete Task")
         print("4. Exit")
+        
 
         choice = input("Enter choice: ")
 
         if choice == "1":
+
             title = input("Enter task title: ")
             manager.add_task(title)
 
         elif choice == "2":
+
             manager.list_tasks()
 
         elif choice == "3":
+
             manager.list_tasks()
-            task_num = int(input("Enter task number to mark complete: "))
-            manager.mark_task_complete(task_num)
+            task_num = int(input("Enter task number to delete: "))
+            manager.delete_task(task_num)
 
         elif choice == "4":
+
             print("Goodbye!")
             break
+
         else:
+
             print("Invalid choice")
 
 
--- task_manager.py
+++ task_manager.py
@@ -2,29 +2,63 @@ import json
 
 
 class TaskManager:
+
     def __init__(self):
         self.tasks = []
+        self.load_tasks()
 
     def add_task(self, title):
+
         task = {
             "title": title,
             "completed": False
         }
+
         self.tasks.append(task)
+        self.save_tasks()
         print("Task added successfully!")
 
     def list_tasks(self):
+
         if not self.tasks:
             print("No tasks available.")
             return
 
         for index, task in enumerate(self.tasks, start=1):
+
             status = "✔" if task["completed"] else "✘"
             print(f"{index}. {task['title']} [{status}]")
-    def mark_task_complete(self, task_number):
+
+    # MEMBER 3 FEATURE
+    # Delete Task
+
+    def delete_task(self, task_number):
+
         if task_number <= 0 or task_number > len(self.tasks):
-            print("Invalid task number.")
+            print("Invalid task number")
             return
 
-        self.tasks[task_number - 1]["completed"] = True
-        print("Task marked as completed!")        
+        removed_task = self.tasks.pop(task_number - 1)
+        self.save_tasks()
+
+        print(f"Deleted task: {removed_task['title']}")
+
+    # MEMBER 3 FEATURE
+    # Save tasks to JSON file
+
+    def save_tasks(self):
+
+        with open("tasks.json", "w") as file:
+            json.dump(self.tasks, file, indent=4)
+
+    # MEMBER 3 FEATURE
+    # Load tasks from JSON
+
+    def load_tasks(self):
+
+        try:
+            with open("tasks.json", "r") as file:
+                self.tasks = json.load(file)
+
+        except FileNotFoundError:
+            self.tasks = []
